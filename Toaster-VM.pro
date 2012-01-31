e763d2babbd060c17671b40e08e6eaf84a15072d
supermaximo93 <supermaximo93@gmail.com>
2012-01-31 17:43:49 +0000

    Initial commit

---
+++ Toaster-VM.pro
@@ -0,0 +1,2 @@
+SOURCES += \
+    ToasterVM.cpp
---
+++ ToasterVM.cpp
@@ -0,0 +1,11 @@
+/*
+ * ToasterVM.cpp
+ *
+ *  Created on: 31 Jan 2012
+ *      Author: Max Foster
+ */
+
+int main(int argc, char * argv[])
+{
+    return 0;
+}
